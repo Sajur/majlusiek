c822772e913c5dcf9f9e93d8584da19094a70f2d
Sajur <szj@vp.pl>
2019-03-13 23:12:37 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+
+const RhythmBot = require('./src/rhythm-bot.js');
+const logger = require('./src/logger.js');
+
+const bot = new RhythmBot({
+    command: {
+        symbol: '!' // command symbol trigger
+    },
+    discord: {
+        token: 'NTU1MTg3Mjc2MzY0MjUxMTQ3.D2sKxw.gQ3b7vjV-pybvWVB-tTaWhKjTIA'//,
+        // manage: {
+        //     channels: [ 
+        //         // Example text channel manager, limits media channel to 5 posts, limit should probably be less than 100 to avoid bugs
+        //         { name: 'media', limit: 5 }  
+        //     ]
+        // }
+    }
+});
+
+bot.connect()
+    .then(() => { 
+        logger.log('Listening');
+        bot.listen();
+    })
+    .catch(err => {
+        logger.error(err);
+    });
